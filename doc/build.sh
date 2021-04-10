e76ddb3bd397919d9ecb26d8f8b6cd5e8594fa40
montythibault-v2 <monty@v2cloud.com>
2021-04-10 11:30:34 -0600

    tmp build script

---
+++ doc/build.sh
@@ -0,0 +1,6 @@
+cd ..
+mvn package
+rm -f ~/guacamole-client/guacamole_home/extensions/*.jar
+cp target/*.jar ~/guacamole-client/guacamole_home/extensions
+catalina run
+# http://localhost:8080/guacamole-1.3.0/#/
--- doc/run.sh
+++ doc/run.sh
@@ -1,13 +1,17 @@
 # Test connection script
 AUTH_KEY=4c0b569e4c96df157eee1b65dd0e4d41
-PROXY_URI=https://proxy-bvhjsx0s.devel02.hazeware.com
+
+# Note that we use /webconnect/api/tokens instead of /guacamole/api/tokens
+#PROXY_URI=https://proxy-bvhjsx0s.devel02.hazeware.com/webconnect/api/tokens
+PROXY_URI=http://localhost:8080/guacamole-1.3.0/guacamole/api/tokens
+
 JSON_FILE=auth.json
 
 
 # This key matched the one in the guacamole home folder
 BASE64=`./encrypt-json.sh $AUTH_KEY $JSON_FILE`
 
-# Note that we use /webconnect/api/tokens instead of /guacamole/api/tokens
-curl --insecure --data-urlencode "data=${BASE64}" $PROXY_URI/webconnect/api/tokens
+
+curl --insecure --data-urlencode "data=${BASE64}" $PROXY_URI
 
 # Curl should log a message with successful connection
